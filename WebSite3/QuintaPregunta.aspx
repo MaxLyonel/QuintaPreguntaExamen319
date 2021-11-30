93056d8bb31a063182a468d998da252e8789084e
MaxLyonel <leorexx100@gmail.com>
2021-11-30 12:38:08 -0400

    Modificado para entrega

---
+++ WebSite3/QuintaPregunta.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="QuintaPregunta.aspx.cs" Inherits="QuintaPregunta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Quinta pregunta Examen INF - 319</title>
+</head>
+<body>
+    <form id="form1" runat="server" method="GET" action="QuintaPregunta.aspx">
+    <div>
+        
+        <h2>QUINTA PREGUNTA EXAMEN INF - 319</h2>
+        Introduzca el número fibonacci a saber:
+        <input id="fibo" type="text" name="fibo" />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 24px; margin-top: 22px" Text="ENVIAR" />
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSite3/QuintaPregunta.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class QuintaPregunta : System.Web.UI.Page
+{
+
+    public int fibonacci(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    } 
+    protected void Page_Load(object sender, EventArgs e)
+    {        
+               
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string fibo = Request.QueryString["fibo"];
+        string resul = Request.QueryString["resul"];
+        int n = Int32.Parse(fibo);
+        Response.Write("<br/>"); 
+        Response.Write("Fibonacci: " + fibonacci(n));
+        
+        
+    }
+}
